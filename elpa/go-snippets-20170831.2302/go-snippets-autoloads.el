5a794a8a11419c7f77aca82ee106233f814acb5b
Shual Liu <liuxulei@ghostcloud.cn>
2018-01-15 16:46:38 +0800

    some

--- elpa/go-snippets-20170831.2302/go-snippets-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; go-snippets-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "go-snippets" "go-snippets.el" (23125 28219
-;;;;;;  475665 885000))
-;;; Generated autoloads from go-snippets.el
-
-(autoload 'go-snippets-initialize "go-snippets" "\
-
-
-\(fn)" nil nil)
-
-(eval-after-load 'yasnippet '(go-snippets-initialize))
-
-;;;***
-
-;;;### (autoloads nil nil ("go-snippets-pkg.el") (23125 28219 497992
-;;;;;;  451000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; go-snippets-autoloads.el ends here
