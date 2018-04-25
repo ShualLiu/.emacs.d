e04f4764e9d85a7cf3d9c69db9ca25cde635cc36
Maurice Aiken <liuxulei@ghostcloud.cn>
2018-04-25 18:39:23 +0800

    optimize

--- elpa/yasnippet-snippets-20180421.204/yasnippet-snippets-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "yasnippet-snippets" "20180421.204" "Collection of yasnippet snippets"
-  '((yasnippet "0.8.0"))
-  :keywords
-  '("snippets"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
